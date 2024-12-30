f7d88cf499e9cb2748428afb8eecc4cc7b02e05d
Yoimiya <yoimiyacyy@gamil.com>
2024-12-30 13:42:37 +0000

    13:42 Dec 30, 2024

--- quickReview.java
+++ quickReview.java
@@ -4,18 +4,14 @@ public class quickReview
 {
     public static void main(String[] args)
     {
-        String str1 = "frustrate";
-        String str2 = str1;
-        String str3 = "frustrate";
-        String str4 = new String("frustrate");
-        boolean test4 = (str1 == str4); // false
-        boolean test1 = (str1 == "frustrate");
-        boolean test2 = (str1 == str2);
-        boolean test3 = (str1 == str3);
-        System.out.println("test1: " + test1);
-        System.out.println("test2: " + test2); 
-        System.out.println("test3: " + test3);
-        System.out.println("test4: " + test4);
+        String str1 = "Computer";
+        String str2 = "Computer";
+        boolean test = (str1 == str2); // true
+        str2 = str1.substring(0,6).concat("ation"); // "Computation"
+        boolean test2 = (str1 == str2); // false
+
+        System.out.println(test);
+        System.out.println(test2);
     }
 
 }
